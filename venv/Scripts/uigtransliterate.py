44d4641fb3cffca868266594403ea2b85dc562e4
Nithya Shree <nithyakl1810@gmail.com>
2026-03-23 22:49:34 +0530

    Applied .gitignore and removed tracked unwanted files

--- venv/Scripts/uigtransliterate.py
+++
@@ -1,9 +0,0 @@
-#!C:\Users\fidel\Desktop\phenomes_analysis\venv\Scripts\python.exe
-
-import fileinput
-import epitran
-
-epi = epitran.Epitran('uig-Arab')
-for line in fileinput.input():
-    s = epi.transliterate(line.strip())
-    print(s)
